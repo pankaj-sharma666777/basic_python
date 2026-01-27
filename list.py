c2d9aca75ae4b9f89bdfd0cb398b74516f40fa56
pankaj-sharma666777 <pankajsharma666777@gmail.com>
2026-01-27 18:01:17 +0530

    fist commit

---
+++ list.py
@@ -0,0 +1,25 @@
+my_list = [1,2]
+print(my_list)
+print(my_list)
+print(my_list)
+print(my_list)
+print(my_list)
+print(type(my_list))
+
+mylist_2 = [1,2,3,"apple","banana",13,25,"a","b",13.5]
+mylist_3 = [1,2,3,[1,2,3],"apple","banana",["apple","banana"]]
+print(type(mylist_2))
+print(type(mylist_3))
+
+
+# tuple
+mytuple = (1,2,3,4,5,7,8)
+print(type(mytuple))
+mytuple_2 =(1,2,3,"apple","banana",13,25,"a","b",13.5)
+print(type(mytuple_2))
+mytuple_3 = (1,2,3,[1,2,3],"apple","banana",["apple","banana",["apple","banana"]])
+mytuple_4 = (5,)
+print(type(mytuple_2),"mytupl_2")
+print(type(mytuple_3),"mytupl_3")
+print(type(mytuple_4),"mytupl_4")
+print(type(mytuple),"mytuple")
---
+++ range1.py
@@ -0,0 +1,6 @@
+range(0,15)
+print(*range(0,15))
+print(*range(15))
+
+for number in range(3,30,2):
+    print(number)
